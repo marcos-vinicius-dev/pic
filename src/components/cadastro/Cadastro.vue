7366b8eb417648360c791e9a89e068fd4d0fddf0
marcos vinicius <marcvinicius.1997@gmail.com>
2019-01-04 14:50:10 -0200

    terceiro commit

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,5 +1,63 @@
 <template>
-    <h2 class="centralizado">Cadastro</h2>
+    <v-container>
+        <form>
+            <v-text-field v-model="name" :error-messages="nameErrors"
+            :counter="10"
+            label="Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+            ></v-text-field>
+
+            <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+            ></v-text-field>
+
+            <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+            ></v-text-field>
+
+            <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+            ></v-text-field>
+
+            <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+            ></v-text-field>
+
+            <v-checkbox
+            v-model="checkbox"
+            :error-messages="checkboxErrors"
+            label="Do you agree?"
+            required
+            @change="$v.checkbox.$touch()"
+            @blur="$v.checkbox.$touch()"
+            ></v-checkbox>
+
+            <v-btn @click="submit">submit</v-btn>
+            <v-btn @click="clear">clear</v-btn>
+        </form>
+    </v-container>
 </template>
 <script>
 export default {
