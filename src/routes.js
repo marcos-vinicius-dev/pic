096f8a7d1cd8ecd94ef2dca846cd488a09c1ad1b
marcosvinicius <marcvinicius.1997@gmail.com>
2019-01-07 13:20:32 -0200

    version 2

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,10 @@ import Home from './components/home/Home.vue';
 import Login from './components/login/Login.vue';
 
 export const routes = [
-    { path: '/', name : 'login', component: Login, titulo : 'Login', icon: 'contacts', meta: { public: true }, menu : false },
-    { path: '/login', name : 'login', component: Login, titulo : 'Login', icon: 'contacts', meta: { public: true }, menu : false },
-    { path: '/home', name : 'home', component: Home, titulo : 'Home', icon: 'contacts', menu : true },
-    { path: '/cadastro', name : 'cadastro', component: Cadastro, titulo : 'Cadastro', icon: 'contacts', menu : true },
-    { path: '/cadastro/:id', name : 'alterar', component: Cadastro, titulo : 'Alterar', icon: 'contacts', menu : false },
+    { path: '/', name : 'login', component: Login, titulo : 'Login', meta: { public: true }, menu : false },
+    { path: '/login', name : 'login', component: Login, titulo : 'Login', meta: { public: true }, menu : false },
+    { path: '/home', name : 'home', component: Home, titulo : 'Home', icon: 'home', menu : true },
+    { path: '/cadastro', name : 'cadastro', component: Cadastro, titulo : 'Cadastro', icon: 'add_box', menu : true },
+    { path: '/cadastro/:id', name : 'alterar', component: Cadastro, titulo : 'Alterar', icon: 'create', menu : false },
     { path: '*', component: Login, meta: { public: true }, menu : false }
 ];
